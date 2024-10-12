a3b6f8b1f90f867a082a094a2a980e2bf08add3f
Saulėnas <73237821+Saulala31@users.noreply.github.com>
2024-10-12 16:23:36 +0300

    fronend

---
+++ Pocarol_backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const UserRoute = require("./routes/user.routes");
+const PostRoute = require("./routes/post.routes");
+const PostRequestRoute = require("./routes/PostRequest.routes");
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.json())
+
+app.use("/",UserRoute);
+
+app.use("/",PostRoute);
+
+app.use("/",PostRequestRoute);
+
+module.exports = app;
+
+
+///to run write node app.js
